0556328674b307c547a23cbc2b8ddf0180603c22
Zosia <zosienkaa@o2.pl>
2018-05-20 18:22:55 +0200

    implemented LinkedStack

---
+++ src/main/java/com/github/grzegorzka/collection/LinkedStack.java
@@ -0,0 +1,66 @@
+package com.github.grzegorzka.collection;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+
+public class LinkedStack<T> implements BasicStack<T> {
+
+   class Node {
+      T value;
+      Node nextNode = null;
+   }
+
+   Node firstNode = null;
+   int count = 0;
+
+   @Override
+   public void push(T object) throws IllegalArgumentException {
+      if (object == null) {
+         throw new IllegalArgumentException("object must be not null");
+      } else {
+         Node node = new Node();
+         node.value = object;
+         if (firstNode == null) {
+            firstNode = node;
+         } else {
+            node.nextNode = firstNode;
+            firstNode = node;
+         }
+         count ++;
+         }
+      }
+
+   @Override
+   public T peek() throws EmptyStackException {
+      if (firstNode == null) {
+         throw new EmptyStackException();
+      } else {
+         return firstNode.value;
+      }
+   }
+
+   @Override
+   public T pop() throws EmptyStackException {
+      if (firstNode == null) {
+         throw new EmptyStackException();
+      } else {
+         T nodeValue = firstNode.value;
+         firstNode = firstNode.nextNode;
+         count --;
+         return nodeValue;
+      }
+   }
+
+   @Override
+   public int size() {
+      return count;
+   }
+
+   @Override
+   public boolean isEmpty() {
+      return firstNode == null;
+   }
+
+
+}
